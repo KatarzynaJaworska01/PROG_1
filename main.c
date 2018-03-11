ea7a640a1f8382149a629968b6c97b8c5f9600dd
KatarzynaJaworska01 <37254387+KatarzynaJaworska01@users.noreply.github.com>
2018-03-11 09:36:25 +0100

    Add files via upload

---
+++ main.c
@@ -0,0 +1,56 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, PHP, Ruby, Perl, Pascal, HTML, CSS, JS
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <stdio.h>
+
+int main()
+{
+     int wiek = 4;
+    short int ilosc_bluzek = 6; 
+    long int ilosc_bulek = 5;
+    signed int pietro = -2;
+    unsigned int pietro2= 3;
+    char punkt = 'e'; 
+    signed char liczba1 = 124; 
+    unsigned char liczba2 = -123; 
+    float temperatura_cieczy = 40.2f;
+    double wysokosc = 1.5687;
+    long double wysokosc2 = 2.1111;
+    short punkty= 8;
+    signed short sid = -9;
+    signed short int der =-7;
+    unsigned short int das = 1;
+    signed die = -2;
+    signed long liczba_czekolady =989834;
+    signed long int liczba_rowerow = 57654;
+    unsigned long int liczba_aut = 5464567569877768;
+    
+    
+    printf("Zmienna int ma rozmiar: %dB\n", sizeof(wiek));
+    printf("Zmienna short int ma rozmiar: %dB\n", sizeof( ilosc_bluzek));
+    printf("Zmienna long int ma rozmiar: %dB\n", sizeof( ilosc_bulek));
+    printf("Zmienna signed int ma rozmiar: %dB\n", sizeof(pietro));
+    printf("Zmienna unsigned ma rozmiar: %dB\n", sizeof(pietro2));
+    printf("Zmienna char ma rozmiar: %dB\n", sizeof(punkt));  
+    printf("Zmienna signed char ma rozmiar: %dB\n", sizeof(liczba1));   
+    printf("Zmienna unsigned char ma rozmiar: %dB\n", sizeof(liczba2));  
+    printf("Zmienna float ma rozmiar: %dB\n", sizeof(temperatura_cieczy)); 
+    printf("Zmienna double ma rozmiar: %dB\n", sizeof(wysokosc));
+    printf("Zmienna long double ma rozmiar: %dB\n", sizeof(wysokosc2));
+    printf("Zmienna short ma rozmiar: %dB\n", sizeof(punkty));
+    printf("Zmienna signed short ma rozmiar: %dB\n", sizeof(sid));
+    printf("Zmienna signed short int ma rozmiar: %dB\n", sizeof(der));
+    printf("Zmienna unsigned short int ma rozmiar: %dB\n", sizeof(das));
+    printf("Zmienna signed ma rozmiar: %dB\n", sizeof(die));
+    printf("Zmienna signed long ma rozmiar: %dB\n", sizeof(liczba_czekolady));
+    printf("Zmienna signed long int ma rozmiar: %dB\n", sizeof(liczba_rowerow));
+    printf("Zmienna unsigned long int ma rozmiar: %dB\n", sizeof(liczba_aut));
+   
+
+
+    return 0;
+}
